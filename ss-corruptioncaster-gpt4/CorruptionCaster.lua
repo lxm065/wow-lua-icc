e1101334e429ff1638c809b2610af9fd400735be
liangxm <151488669@qq.com>
2024-07-16 17:56:34 +0800

    2 commit

---
+++ ss-corruptioncaster-gpt4/CorruptionCaster.lua
@@ -0,0 +1,38 @@
+local isRunning = false;
+
+function CorruptionCaster_OnLoad()
+  print("Corruption Caster Loaded.");
+  CorruptionCasterFrame:Show();
+  CorruptionCasterUI:Show();
+end
+
+function CorruptionCaster_Toggle()
+  isRunning = not isRunning;
+  if isRunning then
+    CorruptionCasterButton:SetText("Stop");
+    CorruptionCasterFrame:RegisterEvent("PLAYER_TARGET_CHANGED");
+    CorruptionCasterFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+  else
+    CorruptionCasterButton:SetText("Start");
+    CorruptionCasterFrame:UnregisterEvent("PLAYER_TARGET_CHANGED");
+    CorruptionCasterFrame:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+  end
+end
+
+function CorruptionCaster_OnEvent(self, event, ...)
+  if event == "PLAYER_TARGET_CHANGED" or event == "COMBAT_LOG_EVENT_UNFILTERED" then
+    if isRunning then
+      CorruptionCaster_CheckAndCastCorruption();
+    end
+  end
+end
+
+function CorruptionCaster_CheckAndCastCorruption()
+  local target = "target";
+  if UnitExists(target) and not UnitDebuff(target, "Corruption") then
+    CastSpellByName("Corruption", target);
+  end
+end
+
+CorruptionCasterFrame:SetScript("OnEvent", CorruptionCaster_OnEvent);
+
